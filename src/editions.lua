cd3f21a16b7d5f0056eb35376f9224b0eae0a5bf
s <136345129+nassymdev@users.noreply.github.com>
2025-08-05 14:40:41 +0100

    Add files via upload

--- src/editions.lua
+++ src/editions.lua
@@ -84,3 +84,120 @@ SMODS.Edition{
 
 
 
+SMODS.Edition{
+    key = "extra",
+    shader = "aao_p_extra",
+    loc_txt={
+        name = "{X:black}Delta",
+        text = {
+            "{C:white,X:green}X#1#{} Mult if {X:tarot,C:white}blinded",
+            "i'll change the effect later "
+        }
+    },
+    config = {
+        extra ={
+            x_mult = 6,
+        },
+    },
+    calculate =  function (self, card, context)
+        card._absolute_deck=true
+        if context.main_scoring and (context.cardarea == G.hand or context.cardarea == G.play) and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+        if context.pre_joker and (context.cardarea == G.jokers)  and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+    end,
+    loc_vars = function (self, info_queue, card)
+        return {
+            vars = {
+                self.config.extra.x_mult,
+            }
+        }
+    end,
+    in_shop = true,
+    weight = 3,
+}
+
+SMODS.Edition{
+    key = "cloud",
+    shader = "aao_p_cloud",
+    loc_txt={
+        name = "{X:black}Delta",
+        text = {
+            "{C:white,X:green}X#1#{} Mult if {X:tarot,C:white}blinded",
+            "i'll change the effect later "
+        }
+    },
+    config = {
+        extra ={
+            x_mult = 6,
+        },
+    },
+    calculate =  function (self, card, context)
+        card._absolute_deck=true
+        if context.main_scoring and (context.cardarea == G.hand or context.cardarea == G.play) and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+        if context.pre_joker and (context.cardarea == G.jokers)  and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+    end,
+    loc_vars = function (self, info_queue, card)
+        return {
+            vars = {
+                self.config.extra.x_mult,
+            }
+        }
+    end,
+    in_shop = true,
+    weight = 3,
+}
+
+SMODS.Edition{
+    key = "glitched",
+    shader = "aao_p_glitched",
+    loc_txt={
+        name = "{X:black}Delta",
+        text = {
+            "{C:white,X:green}X#1#{} Mult if {X:tarot,C:white}blinded",
+            "i'll change the effect later "
+        }
+    },
+    config = {
+        extra ={
+            x_mult = 6,
+        },
+    },
+    calculate =  function (self, card, context)
+        card._absolute_deck=true
+        if context.main_scoring and (context.cardarea == G.hand or context.cardarea == G.play) and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+        if context.pre_joker and (context.cardarea == G.jokers)  and card.facing=="back" then
+            return {
+                Xmult = card.edition.extra.x_mult
+            }
+        end
+    end,
+    loc_vars = function (self, info_queue, card)
+        return {
+            vars = {
+                self.config.extra.x_mult,
+            }
+        }
+    end,
+    in_shop = true,
+    weight = 3,
+}
+
--- src/shaders.lua
+++ src/shaders.lua
@@ -8,3 +8,17 @@ SMODS.Shader{
     key = "delta",
     path = "delta.fs"
 }
+-- test
+SMODS.Shader{
+    key = "extra",
+    path = "extra.fs"
+}
+
+SMODS.Shader{
+    key = "cloud",
+    path = "cloud.fs"
+}
+SMODS.Shader{
+    key = "glitched",
+    path = "glitched.fs"
+}
